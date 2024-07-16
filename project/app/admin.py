150b99fa6ab91a305f6aab73afb155cd62bab15e
Himanshi Mate <matemanu725@gmail.com>
2024-07-16 18:31:58 +0530

    remark

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
 from .models import Student
 # Register your models here.
-admin.site.register(Student)
+admin.site.register(Student) #it is used to show the model on the panel
--- project/app/models.py
+++ project/app/models.py
@@ -8,4 +8,18 @@ class Student(models.Model):
     stu_city=models.TextField(max_length=50)
     stu_enrollment=models.IntegerField(null=True)
     stu_date_of_birth=models.IntegerField()
+
+
+    def __str__(self):
+        return self.stu_name+' '+self.stu_email    
+    
+    class Meta:
+        db_table='student_detail'
+        ordering=['stu_name']        #arranging the name in ascending order 
+       # ordering=['-stu_name']        #arranging the name in decending order 
+        verbose_name='Student'              #it add the 's' in the name which is shown on the  admin panel. 
+        # verbose_name_plural='Student'    #it remove the 's' in the name which is shown on the  admin panel
+    
+
+
     
